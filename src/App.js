739b3cc3ffd29ffe42f92f470a079bf93d1d061e
Jesse <jessemckinney513@gmail.com>
2021-07-02 15:37:03 -0400

    yay

--- src/App.js
+++ src/App.js
@@ -2,7 +2,22 @@ import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
+// const axios = require("axios");
+// const url = "https://www.googleapis.com/youtube/v3/search";
+
 class App extends Component {
+  state = {};
+
+  componentDidMount() {
+    console.log("Mount: Hello World");
+    console.log(process.env.REACT_APP_APIKEY);
+  }
+
+  componentDidUpdate() {
+    console.log("Update: GoodBye World");
+    // console.log(process.env.REACT_APP_APIKEY);
+  }
+
   render() {
     return (
       <>
