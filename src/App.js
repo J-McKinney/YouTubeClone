0b2b8591374ea7905aee6cfefff4fc6c9e2c6fd4
Jesse <jessemckinney513@gmail.com>
2021-07-05 15:02:01 -0400

    I got youtube api working

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
 import React, { Component } from "react";
-import axios from "axios";
+// import axios from "axios";
 import logo from "./logo.svg";
 import "./App.css";
 require("dotenv").config();
-
-// const axios = require("axios");
-const url = "https://www.googleapis.com/youtube/v3/search";
 
 class App extends Component {
   state = {};
 
   componentDidMount() {
     console.log("Mount: Hello World");
-    const getVideo = url + "/neebs&key=" + process.env.REACT_APP_APIKEY;
-    axios
-      .get(getVideo)
-      .then((res) => {
-        return axios.get(getVideo);
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+
+    // const getVideo = axios.get(
+    // `${"https://cors-anywhere.herokuapp.com/"}https://www.googleapis.com/youtube/v3/search`,
+
+    //   `${"https://cors.bridged.cc/"}https://www.googleapis.com/youtube/v3/search`,
+
+    //   {
+    //     params: {
+    //       maxResults: 5,
+    //       q: "neebs",
+    //       key: `${process.env.REACT_APP_APIKEY}`,
+    //     },
+    //   }
+    // );
+    // return getVideo
+    //   .then((res) => {
+    //     console.log(res);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
   }
 
   componentWillUnmount() {
@@ -29,7 +38,6 @@ class App extends Component {
 
   componentDidUpdate() {
     console.log("Update: GoodBye World");
-    // console.log(process.env.REACT_APP_APIKEY);
   }
 
   render() {
