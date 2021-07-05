ee07f99532d6b4a103fbbc5d2457d17a06dfed48
Jesse <jessemckinney513@gmail.com>
2021-07-05 18:05:12 -0400

    yay

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,26 +1,28 @@
 import React from "react";
+import styling from "./SearchBar.module.css";
 
 function SearchBar(props) {
   return (
     <>
-      <form>
-        <label htmlFor="search">Search:</label>
-        <input
-          onChange={props.handleInputChange}
-          value={props.search}
-          name="search"
-          type="text"
-          className="form-control"
-          placeholder="Search for a video"
-          id="search"
-        />
-        <button
-          onClick={props.handleFormSubmit}
-          className="btn btn-primary mt-3"
-        >
-          Search
-        </button>
-      </form>
+      <div className={styling.wrapper}>
+        <form className={styling.form}>
+          <label className={styling.label} htmlFor="search">
+            Search:
+          </label>
+          <input
+            className={styling.formControl}
+            onChange={props.handleInputChange}
+            value={props.search}
+            name="search"
+            type="text"
+            placeholder="Search for a video"
+            id="search"
+          />
+          <button onClick={props.handleFormSubmit} className={styling.button}>
+            Search
+          </button>
+        </form>
+      </div>
     </>
   );
 }
